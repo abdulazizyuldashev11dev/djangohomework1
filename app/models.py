0b9388ddbb9bc1d707c601aa7013ae597d2430b0
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-09-22 20:06:53 +0500

    Finished homework

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from mysite import settings
+
 
 class NewsModel(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     status = models.BooleanField(default=False)
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=2)
     def __str__(self):
         return self.title
